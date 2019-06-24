ffea13c5ce05ddcd672c042236723068b7fad9a5
jesse collier <jesse.collier97@gmail.com>
2019-06-24 15:44:02 +1200

    models

---
+++ BasicWebApp/Controllers/ProductsController.cs
@@ -0,0 +1,29 @@
+﻿using BasicWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BasicWebApp.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ViewCustomer()
+        {
+            Customer customer = new Customer();
+
+            customer.Id = Guid.NewGuid().ToString();
+            customer.Name = "Fred";
+            customer.Telephone = "123456";
+
+            return View(customer);
+        }
+    }
+}
